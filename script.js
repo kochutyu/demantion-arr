31ea2a779f03f2703e9add9289f5ab8933161716
Kochut Yura <56127664+kochutyu@users.noreply.github.com>
2020-06-22 19:30:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 class twoDimensionArray {
     arr = [];
+
     constructor(width, height) {
         this.width = width;
         this.height = height;
     }
 
     init() {
-
         for (let i = 0; i < this.width; i++) {
             this.arr[i] = [];
             for (var j = 0; j < this.height; j++) {
@@ -14,8 +14,8 @@ class twoDimensionArray {
             }
         }
         return this.arr
-
     }
+
 }
 
 const newArr = new twoDimensionArray(10, 10);
